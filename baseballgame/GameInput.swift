88474683b9ee78c731262ad9537ed1c05ef01af5
ybin4548 <ybin4548@gmail.com>
2026-01-15 14:04:59 +0900

    수정 : GameInput ( guard let 구문)

--- baseballgame/GameInput.swift
+++ baseballgame/GameInput.swift
@@ -14,8 +14,7 @@ struct GameInput {
     func getInput() -> [Int]? {
         /* (Lv.2) 1. 사용자 입력 검증 */
         print("숫자를 입력하세요: ", terminator: "\n")
-        guard let input = readLine(),
-              let num = Optional(input.compactMap{Int(String($0))}), //숫자 외의 다른 값이 들어왔는지 확인, map 대신 compactMap 사용 : map 사용시 nil로 처리 되기 때문에 처리가 바르게 일어나지않음(ex. 1ab입력시 num = [1,nil,nil] -> 중복으로 처리됨
+        guard let num = readLine().map({ $0.compactMap(\.wholeNumberValue) }), //숫자 외의 다른 값이 들어왔는지 확인, map 대신 compactMap 사용 : map 사용시 nil로 처리 되기 때문에 처리가 바르게 일어나지않음(ex. 1ab입력시 num = [1,nil,nil] -> 중복으로 처리됨
               num.count == 3
         else {
             print("올바르지 않은 입력값입니다.")
